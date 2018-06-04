3f0c4043d0176fb328eb65d5cf9e6f2cbcc69537
Chris Purta <cpurta@kochava.com>
2018-06-04 16:31:11 -0700

    Initial commit

---
+++ raiden.rb
@@ -0,0 +1,20 @@
+class Raiden < Formula
+  desc "Raiden Network"
+  homepage "https://github.com/raiden-network/homebrew-raiden"
+  url "https://github.com/raiden-network/raiden.git", :tag => 'v0.3.0'
+
+  depends_on :mac_os => :el_capitan
+
+  depends_on :python => :build
+  depends_on :pip => :build
+
+  def install
+    system "cd", "raiden"
+    system "pip", "install", "--upgrade", "-r", "requirements-dev.txt"
+    system "python", "setup.py", "develop"
+  end
+
+  test do
+    system "#{HOMEBREW_PREFIX}/bin/raiden", "version"
+  end
+end
