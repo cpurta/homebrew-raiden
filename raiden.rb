63677e3354bff08a5f5229e1ab2adaf834153974
Chris Purta <cpurta@kochava.com>
2018-06-05 08:38:42 -0700

    Update paths to file and use bin.install

--- raiden.rb
+++ raiden.rb
@@ -3,11 +3,14 @@ class Raiden < Formula
   homepage "https://github.com/cpurta/homebrew-raiden"
   url "https://github.com/raiden-network/raiden/releases/download/v0.3.0/raiden-v0.3.0-macOS.zip"
   version "v0.3.0"
+  checksum "3f34d241f6b23d30d69c7bd25fa62de280e5f2bb34f9c08340f3e390e80e2809"
 
   depends_on :macos => :el_capitan
 
   def install
-    system "unzip", "raiden-v0.3.0-macOS.zip"
+    system "unzip", "#{HOMEBREW_PREFIX}/raiden-v0.3.0-macOS.zip"
+    system "mv", "#{HOMEBREW_PREFIX}/raiden-v0.3.0-macOS", "#{HOMEBREW_PREFIX}/raiden"
+    bin.install "raiden"
   end
 
   test do
