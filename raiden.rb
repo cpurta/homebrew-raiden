30ab0cf83ef485407899dfb4104f7b9d6e41d394
Chris Purta <cpurta@kochava.com>
2018-06-05 09:51:43 -0700

    Use mac and update file name

--- raiden.rb
+++ raiden.rb
@@ -1,16 +1,16 @@
 class Raiden < Formula
   desc "Raiden Network"
   homepage "https://github.com/cpurta/homebrew-raiden"
-  url "https://github.com/raiden-network/raiden/releases/download/v0.3.0/raiden-v0.3.0-linux.tar.gz"
+  url "https://github.com/raiden-network/raiden/releases/download/v0.3.0/raiden-v0.3.0-macOS.zip"
   version "v0.3.0"
 
   depends_on :macos => :el_capitan
 
   def install
-    bin.install "raiden-v0.3.0"
+    bin.install "raiden-0.3.0-macos"
   end
 
   test do
-    system "#{bin}/raiden", "version"
+    system "#{bin}/raiden-0.3.0-macos", "version"
   end
 end
