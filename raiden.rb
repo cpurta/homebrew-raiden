16584eee1bd9f1b0ee72d1b8234a3c01adabf801
Chris Purta <cpurta@kochava.com>
2018-06-05 09:56:34 -0700

    Add checksum, rename binary

--- raiden.rb
+++ raiden.rb
@@ -3,14 +3,16 @@ class Raiden < Formula
   homepage "https://github.com/cpurta/homebrew-raiden"
   url "https://github.com/raiden-network/raiden/releases/download/v0.3.0/raiden-v0.3.0-macOS.zip"
   version "v0.3.0"
+  sha256 "3f34d241f6b23d30d69c7bd25fa62de280e5f2bb34f9c08340f3e390e80e2809"
 
   depends_on :macos => :el_capitan
 
   def install
-    bin.install "raiden-0.3.0-macos"
+    system "mv", "raiden-0.3.0-macos", "raiden"
+    bin.install "raiden"
   end
 
   test do
-    system "#{bin}/raiden-0.3.0-macos", "version"
+    system "#{bin}/raiden", "version"
   end
 end
