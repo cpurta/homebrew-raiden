fb668002a63cfc22704351f274a05820ffe305dc
Chris Purta <cpurta@kochava.com>
2018-06-05 09:32:16 -0700

    remove sha256 for now

--- raiden.rb
+++ raiden.rb
@@ -3,7 +3,6 @@ class Raiden < Formula
   homepage "https://github.com/cpurta/homebrew-raiden"
   url "https://github.com/raiden-network/raiden/releases/download/v0.3.0/raiden-v0.3.0-linux.tar.gz"
   version "v0.3.0"
-  sha256 "3f34d241f6b23d30d69c7bd25fa62de280e5f2bb34f9c08340f3e390e80e2809"
 
   depends_on :macos => :el_capitan
 
